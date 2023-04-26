a7292421aaadb1cab97ef7b2b3b4beacc4859aa8
Estrella Mooney <estrellamooney96@gmail.com>
2023-04-26 15:38:38 +1200

    Main section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@component/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import '../styles/globals.css'
+import Header from './header'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  return (
+    <>
+      <h1>EstrellaSite</h1>
+      <Header />
+      <Component {...pageProps} />
+      <p>Footer</p>
+    </>
+  )
 }
---
+++ pages/header.tsx
@@ -0,0 +1,28 @@
+import { AppProps } from 'next/app'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+export default function Header() {
+  const router = useRouter()
+  return (
+    <>
+      <nav className="header-nav">
+        <ul>
+          <li>
+            <Link className={router.pathname == '/' ? 'active' : ''} href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={router.pathname == '/about' ? 'active' : ''}
+              href="/about"
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
