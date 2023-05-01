bf5f58af846aa1c4fee10065fefcbcdeb9ad5c0f
Estrella Mooney <estrellamooney96@gmail.com>
2023-05-01 13:40:19 +1200

    fix error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,3 @@
-// import { AppProps } from 'next/app'
-// import { useRouter } from 'next/router'
-// import '../styles/globals.css'
-// import Header from './header'
-
-// export default function App({ Component, pageProps }: AppProps) {
-//   const router = useRouter()
-//   return (
-//     <>
-//       <h1 className="text-2xl font-semibold">EstrellaSite</h1>
-//       <Header />
-//       <Component {...pageProps} />
-//       <p>Footer</p>
-//     </>
-//   )
-// }
-
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
