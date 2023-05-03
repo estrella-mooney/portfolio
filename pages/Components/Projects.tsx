7bb983b73a52baf28ce66607d66ad713de38edd4
Estrella Mooney <estrellamooney96@gmail.com>
2023-05-03 15:33:17 +1200

    Create wisdom as project

--- pages/Components/Projects.tsx
+++ pages/Components/Projects.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import wanderLust from '../../public/wanderlust.png'
 import buddy from '../../public/buddy.png'
+import wisdom from '../../public/wisdom.png'
 
 function Projects() {
   return (
@@ -87,9 +88,13 @@ function Projects() {
             <h1 className="text-lg font-semibold mb-5">Buddy</h1>
             <p className="font-light mb-5">
               An app helping newcomers in Wellington, NZ connect with locals for
+              cultural immersion and guidance.
+            </p>{' '}
+            {/* <p className="font-light mb-5">
+              An app helping newcomers in Wellington, NZ connect with locals for
               cultural immersion and guidance. (Final Group project at Dev
               Academy).
-            </p>
+            </p> */}
             <p className="mb-5">
               <b>Tech Used:</b>
             </p>
@@ -130,6 +135,59 @@ function Projects() {
                   <p className="inline-block">View Buddy</p>
                 </a>
               </div>
+            </div>
+          </div>
+
+          <div className="bg-indigo-200 dark:hover:bg-gray-600 flex-1 p-7 my-6 rounded-lg max-w-xl hover:shadow-lg transition ease-in-out duration-200 hover:text-contrast-white text-gray-800 dark:bg-gray-700 dark:text-gray-300">
+            <Image
+              alt="yes"
+              src={wisdom}
+              width={400}
+              height={215}
+              decoding="async"
+              className="mb-3 rounded-lg"
+              loading="lazy"
+            ></Image>
+            <h1 className="text-lg font-semibold mb-5">Wisdom Of Zoltash</h1>
+            <p className="font-light mb-5">
+              Discover what the future holds with Wisdom of Zoltash - your
+              ultimate fortune-telling app.
+            </p>
+            <p className="mb-5">
+              <b>Tech Used:</b>
+            </p>
+            <p className="mb-5">Handlebars, CSS </p>
+            <p>
+              <b>Reflections:</b>
+            </p>
+            <p className="mb-5">
+              This project was the very first group project I worked on at Dev
+              Academy, where we had just one week to learn how to use
+              Handlebars. While we successfully completed the project using
+              Handlebars, we transitioned to learning React in the following
+              week. Now, I would like to take this project to the next level by
+              upgrading it with React and Next.js, to provide a better user
+              experience with added functionality.
+            </p>
+            <div className="flex items-center">
+              <a
+                href="https://github.com/estrella-mooney/wisdom-of-zoltash"
+                title="GitHub"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="octicon octicon-mark-github pr-1"
+                  height="24"
+                  version="1.1"
+                  viewBox="0 0 16 16"
+                  width="24"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                  ></path>
+                </svg>
+              </a>
             </div>
           </div>
         </div>
