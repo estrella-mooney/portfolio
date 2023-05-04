bef53c73df20d3f66a75298ee8aa8210561c34f7
Estrella Mooney <estrellamooney96@gmail.com>
2023-05-04 16:18:48 +1200

    Chaning name

--- pages/Components/Page.tsx
+++ pages/Components/Page.tsx
@@ -1,13 +1,21 @@
-async function getPosts() {
-  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`)
+import { getPosts } from '.././GetPosts'
+
+export async function getPosts() {
+  console.log('Fetching posts...')
+  const res = await fetch('http://localhost:3000/api/getPosts')
   if (!res.ok) {
-    console.log(res)
+    console.log('Response status', res.status)
   }
-  return res.json()
+  const data = await res.json()
+  console.log('API response:', data)
+  return data
 }
 
 export default async function Home() {
   const data = await getPosts()
+  console.log('Received data:', data)
+
+  console.log(data)
   console.log('testing to see if data passes through', data)
   return (
     <>
@@ -15,3 +23,24 @@ export default async function Home() {
     </>
   )
 }
+
+// async function getPosts() {
+//   const res = await fetch('http://localhost:3000/api/getPosts')
+//   if (!res.ok) {
+//     console.log('Response status', res.status)
+//   }
+//   return res.json()
+// }
+
+// export default async function Home() {
+//   const data = await getPosts()
+
+//   // Output the data in a formatted way
+//   console.log(JSON.stringify(data, null, 2))
+
+//   return (
+//     <>
+//       <h1>Hell this is a test</h1>
+//     </>
+//   )
+// }
--- pages/api/YAS.ts
+++ pages/api/YAS.ts
@@ -1,10 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../prisma/client'
 
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log('API endpoint hit')
+
   try {
     const data = await prisma.post.findMany()
     return res.status(200).json(data)
