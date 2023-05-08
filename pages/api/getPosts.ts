9177479aad0c8a468ff71b2b09469bbb31a5a8ac
Estrella Mooney <estrellamooney96@gmail.com>
2023-05-09 09:28:58 +1200

    Correct issue

--- pages/api/getPosts.ts
+++ pages/api/getPosts.ts
@@ -1,27 +1,9 @@
-// import type { NextApiRequest, NextApiResponse } from 'next'
-// import prisma from '../../prisma/client'
-
-// export default async function getPostsAPI(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   console.log('API endpoint hit')
-
-//   try {
-//     const data = await prisma.post.findMany()
-//     return res.status(200).json(data)
-//   } catch (error: any) {
-//     console.error('Error fetching posts:', error.message)
-//     return res.status(500).json({ message: 'Internal server error' })
-//   }
-// }
-
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../prisma/client'
 
 export default async function getPostsAPI(
-  req: VercelRequest,
-  res: VercelResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
   console.log('API endpoint hit')
 
