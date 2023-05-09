8c84af0773b121c9526d97f1c1930dae8dca3f76
Estrella Mooney <estrellamooney96@gmail.com>
2023-05-09 16:21:24 +1200

    Create univer svg -> fix links

--- pages/Components/MainProjects.tsx
+++ pages/Components/MainProjects.tsx
@@ -4,6 +4,22 @@ import buddy from '../../public/buddy.png'
 import wisdom from '../../public/wisdom.png'
 
 export default function MainProjects() {
+  const githubIcon = (
+    <svg
+      aria-hidden="true"
+      className="octicon octicon-mark-github pr-1"
+      height="24"
+      version="1.1"
+      viewBox="0 0 16 16"
+      width="24"
+    >
+      <path
+        fillRule="evenodd"
+        fill="white"
+        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+      ></path>
+    </svg>
+  )
   return (
     <>
       <section className="py-10">
@@ -52,26 +68,18 @@ export default function MainProjects() {
                 </p>
 
                 <div className="flex items-center">
-                  <a href="https://github.com/kahu-2023/buddy" title="GitHub">
-                    <svg
-                      aria-hidden="true"
-                      className="octicon octicon-mark-github pr-1"
-                      height="24"
-                      version="1.1"
-                      viewBox="0 0 16 16"
-                      width="24"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        fill="white"
-                        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-                      ></path>
-                    </svg>
+                  <a
+                    href="https://github.com/estrella-mooney/wanderLust"
+                    title="GitHub"
+                    target="_blank"
+                  >
+                    {githubIcon}
                   </a>
                   <div className="pl-1">
                     <a
                       href="https://wanderlust-estrella.devacademy.nz/"
                       title="Buddy"
+                      target="_blank"
                     >
                       <p className="inline-block">View Wunderlust</p>
                     </a>
@@ -121,27 +129,13 @@ export default function MainProjects() {
                 </p>
 
                 <div className="flex items-center">
-                  <a href="https://github.com/kahu-2023/buddy" title="GitHub">
-                    <svg
-                      aria-hidden="true"
-                      className="octicon octicon-mark-github pr-1"
-                      height="24"
-                      version="1.1"
-                      viewBox="0 0 16 16"
-                      width="24"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        fill="white"
-                        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-                      ></path>
-                    </svg>
+                  <a
+                    href="https://github.com/kahu-2023/buddy"
+                    title="GitHub"
+                    target="_blank"
+                  >
+                    {githubIcon}
                   </a>
-                  <div className="pl-1">
-                    <a href="https://buddy.devacademy.nz/" title="Buddy">
-                      <p className="inline-block">View Buddy</p>
-                    </a>
-                  </div>
                 </div>
               </div>
             </div>
@@ -188,21 +182,9 @@ export default function MainProjects() {
                   <a
                     href="https://github.com/estrella-mooney/wisdom-of-zoltash"
                     title="GitHub"
+                    target="_blank"
                   >
-                    <svg
-                      aria-hidden="true"
-                      className="octicon octicon-mark-github pr-1"
-                      height="24"
-                      version="1.1"
-                      viewBox="0 0 16 16"
-                      width="24"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        fill="white"
-                        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-                      ></path>
-                    </svg>
+                    {githubIcon}
                   </a>
                 </div>
               </div>
